7a247cbf13aeb9cf4aeefad365c723fcbde5b832
Imesh Nirmal <imeshnirmal1u@gmail.com>
2025-06-07 21:30:09 +0530

    Update API base URL in config.js; comment out local development URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://testbackendsudeepa.azurewebsites.net/api/v1";
 export const token = localStorage.getItem("token");
